0f58bbe31b716df802adc74d01fb77913258c15e
Argenis Chirinos <Argenis_Chirinos@gap.com>
2019-07-07 17:08:30 -0400

    Small changes

--- tottus/extractors.py
+++ tottus/extractors.py
@@ -3,27 +3,33 @@ from .models import ProductInformation
 def get_products(html):
   return html.find_all('div', class_ = 'item-product-caption')
 
-def get_regular_price(price):
+def get_regular_price(product):
+  price = product.find('span', class_ = 'nule-price')
   if not price:
     return 0
   return price.text.strip()
 
-def get_current_price(price):
+def get_current_price(product):
+  price = product.find('span', class_ = 'active-price')
   return price.span.text.strip()
 
-def get_product_name(title):
+def get_product_name(product):
+  title = product.find('div', class_ = 'title')
   return title.a.h5.div.text.strip()
 
-def get_measure(statement):
+def get_measure(product):
+  statement = product.find('div', class_ = 'statement')
   return statement.text.strip()
 
-def get_promotions(offer_details):
+def get_promotions(product):
+  offer_details = product.find('div', class_ = 'offer-details')
   promotions = offer_details.find('span', class_ = 'red')
   if not promotions:
     return 'N/A'
   return promotions.text.strip()
 
-def get_promotions_period(offer_details):
+def get_promotions_period(product):
+  offer_details = product.find('div', class_ = 'offer-details')
   promotions = offer_details.find('span', class_ = 'conditions')
   if not promotions:
     return 'N/A'
@@ -31,12 +37,12 @@ def get_promotions_period(offer_details):
   return period.strip()
 
 def extract_product_information(product):
-  name = get_product_name(product.find('div', class_ = 'title'))
-  regular_price = get_regular_price(product.find('span', class_ = 'nule-price'))
-  current_price = get_current_price(product.find('span', class_ = 'active-price'))
-  measure = get_measure(product.find('div', class_ = 'statement'))
-  promotions = get_promotions(product.find('div', class_ = 'offer-details'))
-  promotions_period = get_promotions_period(product.find('div', class_ = 'offer-details'))
+  name = get_product_name(product)
+  regular_price = get_regular_price(product)
+  current_price = get_current_price(product)
+  measure = get_measure(product)
+  promotions = get_promotions(product)
+  promotions_period = get_promotions_period(product)
   product_information = ProductInformation(name, regular_price, current_price, measure, promotions, promotions_period)
   return product_information
 
