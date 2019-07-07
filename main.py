ca039a75b84b01e3a43b50974bbde5caecab67db
Argenis Chirinos <Argenis_Chirinos@gap.com>
2019-07-07 15:22:21 -0400

    Small change

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from pathlib import Path
 
 path = Path().absolute()
 
-tottus_entries = load_entries('{}/tottus_entries.json'.format(path))
-lider_buysmart_entries = load_entries('{}/lider_smartbuy_entries.json'.format(path))
-lider_supermarket_entries = load_entries('{}/lider_supermarket_entries.json'.format(path))
+tottus_entries = load_entries('{}/entries/tottus_entries.json'.format(path))
+lider_buysmart_entries = load_entries('{}/entries/lider_smartbuy_entries.json'.format(path))
+lider_supermarket_entries = load_entries('{}/entries/lider_supermarket_entries.json'.format(path))
 
 tottus.scrap(tottus_entries)
 lider.buysmart.scrap(lider_buysmart_entries)
