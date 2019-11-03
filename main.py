c62cd37e52883d49c43efc557f09e7108f47b399
Argenis Chirinos <Argenis_Chirinos@gap.com>
2019-11-03 13:40:56 -0300

    changes

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ def start():
   lider_buysmart_entries = load_entries('{}/entries/lider_smartbuy_entries.json'.format(path))
   lider_supermarket_entries = load_entries('{}/entries/lider_supermarket_entries.json'.format(path))
   selcobrand_entries = load_entries('{}/entries/selcobrand_entries.json'.format(path))
-  #scrapers.tottus.scrap(tottus_entries)
-  #scrapers.lider.buysmart.scrap(lider_buysmart_entries)
-  #scrapers.lider.supermarket.scrap(lider_supermarket_entries)
-  scrapers.selcobrand.scrap(selcobrand_entries)
+  scrapers.tottus.scrap(tottus_entries)
+  scrapers.lider.buysmart.scrap(lider_buysmart_entries)
+  scrapers.lider.supermarket.scrap(lider_supermarket_entries)
+  #scrapers.selcobrand.scrap(selcobrand_entries)
 
 if __name__ == '__main__':
   main()
